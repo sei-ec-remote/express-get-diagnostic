d473cdf4e934edcb7de7590603ccc4ff17bc8551
eshalali <eshal.a.ali@gmail.com>
2022-06-30 08:28:19 -0500

    Eshal Ali - -Finished diagnostic

--- exercises/express-get.js
+++ exercises/express-get.js
@@ -18,7 +18,7 @@ Question 1
 Send a response containing a string of `hello world`
 */
 function helloWorld(req, res) {
-	// answer here
+	res.send('hello world')
 }
 router.get('/', helloWorld)
 
@@ -28,7 +28,7 @@ Question 2
 Using the request param of `:name`, send a response containing `hello :name` making sure to replace the `:name` with the request param. 
 */
 function sendBackParams(req, res) {
-	// answer here
+	res.send(`hello ${req.params.name}`)
 }
 router.get('/:name', sendBackParams)
 
@@ -38,7 +38,8 @@ Question 3
 Using the request param of `:num`, mulitply that number by 2 then send a response of the product.
 */
 function mulitplyByTwo(req, res) {
-	// answer here
+	let multNum = Number(req.params.num) * 2
+	res.send(`${multNum}`)
 }
 router.get('/cal/:num', mulitplyByTwo)
 
